6662865d66d8275f1e29d7473c37a41a88f61920
ssooftt <ssooftt@users.noreply.github.com>
2021-06-04 13:53:00 -0300

    Changed version on `fxmanifest.lua` to `2.0`

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Soft#6666'
 description 'AntiBug de disparos a traves de paredes | Shooting through walls AntiBug'
-version '1.0'
+version '2.0'
 
 client_scripts {
 	'config.lua',
